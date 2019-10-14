6654aa06ce1a090c215b08e43996124e83835500
James Dordoy <jamesdordoy@gmail.com>
2019-10-14 12:35:25 +0100

    Initial Commit

---
+++ BasicNeuralNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BasicNeuralNet
+//
+//  Created by James Dordoy on 14/10/2019.
+//  Copyright © 2019 James Dordoy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
